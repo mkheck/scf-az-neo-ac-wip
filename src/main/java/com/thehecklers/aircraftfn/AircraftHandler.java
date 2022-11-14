1311e353692656b5b8925522757383198cc87fee
Mark Heckler <mark.heckler@gmail.com>
2022-11-14 11:15:46 -0600

    Cleanup

--- src/main/java/com/thehecklers/aircraftfn/AircraftHandler.java
+++ src/main/java/com/thehecklers/aircraftfn/AircraftHandler.java
@@ -8,26 +8,6 @@ import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
 
 import java.util.Optional;
 
-//public class AircraftHandler extends FunctionInvoker<Aircraft, String> {
-//    @FunctionName("ac")
-//    public HttpResponseMessage execute(
-//            @HttpTrigger(name = "request",
-//                    methods = {HttpMethod.GET, HttpMethod.POST},
-//                    authLevel = AuthorizationLevel.ANONYMOUS)
-//                    HttpRequestMessage<Optional<Aircraft>> request,
-//            ExecutionContext context) {
-//        Aircraft ac = request.getBody()
-//                .filter(a -> a.adshex() != null)
-//                .orElseGet(() -> new Aircraft("000000", "123456", "UNK"));
-//        context.getLogger().info("Aircraft ID: " + ac.adshex());
-//        System.out.println("<> Aircraft ID: " + ac.adshex());
-//        return request
-//                .createResponseBuilder(HttpStatus.OK)
-//                .body(handleRequest(ac, context))
-//                .header("Content-Type", "application/json")
-//                .build();
-//    }
-//}
 public class AircraftHandler extends FunctionInvoker<Aircraft, Aircraft> {
     @FunctionName("ac")
     public HttpResponseMessage execute(
@@ -37,7 +17,8 @@ public class AircraftHandler extends FunctionInvoker<Aircraft, Aircraft> {
                     HttpRequestMessage<Optional<Aircraft>> request,
             ExecutionContext context) {
         Aircraft aircraft = request.getBody()
-                        .orElseGet(() -> new Aircraft("010101", "987654", "PA28"));
+//                .filter(a -> a.getAdshex() != null)
+                .orElseGet(() -> new Aircraft("010101", "987654", "UNK"));
         context.getLogger().info("Aircraft ID is " + aircraft.getAdshex());
         return request
                 .createResponseBuilder(HttpStatus.OK)
